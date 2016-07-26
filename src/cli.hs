238912bfe4a8ee3da80c57c5a23e51c9888742c7
cpselvis <cpselvis@gmail.com>
2016-07-26 10:19:09 +0800

    arithmetic expression parser

--- src/cli.hs
+++ src/cli.hs
@@ -7,9 +7,13 @@ This file implements a REPL /shell/ to host the interpreter, and also
 allows execution of stand-alone files containing Scheme code.
 -}
 
-main :: IO ()
-main = showBanner
-
+-- Module Dependencies
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+import Lang.Scheme.Types
+import Lang.Scheme.Eval
+import Lang.Scheme.Parser
+import Lang.Scheme.Numerical
 
 -- |A utility function to display the husk console banner
 showBanner :: IO ()
@@ -24,3 +28,15 @@ showBanner = do
     putStrLn " https://github.com/cpselvis/scheme-in-hashell                            "
     putStrLn " (c) 2016 Elvis                                                           "
     putStrLn ""
+
+-- Read expression
+readExpr :: String -> LispVal
+readExpr input = case parse parseExpr "lisp" input of
+    Left err -> String $ "No match: " ++ show err
+    Right val -> val
+
+-- main
+main :: IO()
+main = getArgs >>= putStrLn .show .eval .readExpr .(!! 0)
+
+instance Show LispVal where show = showVal
--- src/lang/scheme/Numerical.hs
+++ src/lang/scheme/Numerical.hs
@@ -7,17 +7,33 @@
  This module implements number operator such add, sub, multi, div and so on.
  Currently, only support Integer type (Float, Complex, Rational hasn't support yet)
 -}
-
 module Lang.Scheme.Numerical
     (
-      numAdd
-    , numSub
-    , numMul
-    , numDiv
-    , numMod
+      primitives
     )
     where
 
 import Lang.Scheme.Types
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [("+", numericBinop (+)),
+              ("-", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop div),
+              ("mod", numericBinop mod),
+              ("quotient", numericBinop quot),
+              ("remainder", numericBinop rem)]
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) = let parsed = reads n in
+                         if null parsed
+                           then 0
+                           else fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
 
 
